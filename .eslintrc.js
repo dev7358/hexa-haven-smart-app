e7facaa152b3d9ae1002bfc7dc7523a1e254c5b8
Vijay-dk28 <=>
2025-01-25 14:38:31 +0530

    reverted the changes

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,4 @@
-const path = require('path');
-
 module.exports = {
   root: true,
-  extends: '@react-native',
-  plugins: ['eslint-rules'],
-  settings: {
-    'eslint-rules': './eslint-rules',
-  },
-  overrides: [
-    {
-      files: ['src/features/**/*.*'], // Target all files in src/features
-      plugins: ['custom-rules'], // Add a custom plugin
-      rules: {
-        'custom-rules/hexa-prefix': 'error', // Enable the custom rule
-      },
-    },
-  ],
+  extends: '@react-native'
 };
