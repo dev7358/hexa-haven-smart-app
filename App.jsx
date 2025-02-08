8ad680db889acfea764e47cc99148e85a10cd6c4
Vijay-dk28 <=>
2025-02-08 17:12:29 +0530

    added route

--- App.jsx
+++ App.jsx
@@ -1,6 +1,14 @@
-import HexaDashboard from './src/features/HexaDashboard';
-import { Provider } from 'react-redux';
+import {Provider} from 'react-redux';
 import store from './src/redux/store';
+import { NavigationContainer } from "@react-navigation/native";
+import Routes from "./src/routes/Route";
+
 export default function App() {
-  return  <Provider store={store}><HexaDashboard /></Provider>;
+  return (
+    <Provider store={store}>
+      <NavigationContainer>
+        <Routes />
+      </NavigationContainer>
+    </Provider>
+  );
 }
